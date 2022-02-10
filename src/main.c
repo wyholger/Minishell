d8834c9f0135093ff54ef1e5f1403f05da8be34d
Dan <dan154v@gmail.com>
2022-02-10 23:33:08 +0300

    add export with no argument

--- src/main.c
+++ src/main.c
@@ -256,62 +256,134 @@ void	cd(t_data *data, t_info *tmp)
 		free(path);
 }
 
-//t_list	*sort_env(t_data *data, t_info *tmp)
-//{
-//	t_list	*res;
-//
-//
-//}
-//
-//void	export_if_no_arg(t_data *data, t_info *tmp)
-//{
-//	t_list	*tmp_env;
-//
-//	tmp_env = data->env;
-//	while (tmp_env != NULL)
-//	{
-//		tmp_env->value = 0;
-//		tmp_env = tmp_env->next;
-//	}
-//	tmp_env = data->env;
-//	while ()
-//}
+void	swap_token(t_list	*swap_token)
+{
+	if (swap_token->prev != NULL && swap_token->next != NULL && swap_token->next->next != NULL)
+	{
+		swap_token->next->prev = swap_token->prev;
+		swap_token->prev->next = swap_token->next;
+		swap_token->next->next->prev = swap_token;
+		swap_token->prev = swap_token->next;
+		swap_token->next = swap_token->next->next;
+		swap_token->prev->next= swap_token;
+	}
+	else if (swap_token->prev == NULL && swap_token->next != NULL && swap_token->next->next != NULL)
+	{
+		swap_token->next->prev = swap_token->prev;
+		swap_token->next->next->prev = swap_token;
+		swap_token->prev = swap_token->next;
+		swap_token->next = swap_token->next->next;
+		swap_token->prev->next= swap_token;
+	}
+	else if (swap_token->prev != NULL && swap_token->next != NULL && swap_token->next->next == NULL)
+	{
+		swap_token->next->prev = swap_token->prev;
+		swap_token->prev->next = swap_token->next;
+		swap_token->prev = swap_token->next;
+		swap_token->next = swap_token->next->next;
+		swap_token->prev->next= swap_token;
+	}
+}
+
+void 	print_env_for_export(t_list *lst)
+{
+	char	**split;
+
+	while (lst != NULL)
+	{
+		ft_putstr_fd("declare -x ", 1);
+		split = ft_split(lst->word, '=');
+		ft_putstr_fd(split[0], 1);
+		ft_putstr_fd("=\"", 1);
+		ft_putstr_fd(split[1], 1);
+		ft_putstr_fd("\"\n", 1);
+		split_free(split);
+		lst = lst->next;
+	}
+}
+
+void	export_if_no_arg(t_data *data)
+{
+	t_list	*tmp_env;
+	t_list	*tmp;
+	t_list	*new;
+	int		size;
+
+	new = NULL;
+	size = ft_lstsize(data->env);
+	tmp_env = data->env;
+	while (tmp_env != NULL)
+	{
+		ft_lstadd_back(&new, ft_lstnew(ft_strdup(tmp_env->word)));
+		tmp_env = tmp_env->next;
+	}
+	while (size >= 0)
+	{
+		while (new->prev != NULL)
+			new = new->prev;
+		tmp = new;
+		while (tmp != NULL && tmp->next != NULL)
+		{
+			if (ft_strcmp(tmp->word, tmp->next->word) > 0)
+			{
+				swap_token(tmp);
+				tmp = tmp->prev;
+			}
+			tmp = tmp->next;
+		}
+		size--;
+	}
+	while (new->prev != NULL)
+		new = new->prev;
+	print_env_for_export(new);
+	ft_lstclear(&new);
+}
 
 void	export(t_data *data, t_info *tmp)
 {
 	char	**for_split;
 	t_list	*tmp_env;
+	int 	i;
+	int		flag;
 
-
+	i = 1;
+	flag = 0;
 	if (tmp->arg[1] != NULL)
 	{
-		if (ft_isalpha(tmp->arg[1][0]) == 1 || tmp->arg[1][0] == '_')
+		while (tmp->arg[i])
 		{
-			for_split = ft_split(tmp->arg[1], '=');
-			tmp_env = search_token_in_envp(data, for_split[0]);
-			if (tmp_env == NULL)
+			if (ft_isalpha(tmp->arg[i][0]) == 1 || tmp->arg[i][0] == '_')
 			{
-				ft_lstadd_back(&data->env, ft_lstnew(ft_strdup(tmp->arg[1])));
+				for_split = ft_split(tmp->arg[i], '=');
+				tmp_env = search_token_in_envp(data, for_split[0]);
+				if (tmp_env == NULL)
+				{
+					ft_lstadd_back(&data->env, ft_lstnew(ft_strdup(tmp->arg[i])));
+				}
+				else
+				{
+					ft_lstdelone(&tmp_env);
+					ft_lstadd_back(&data->env, ft_lstnew(ft_strdup(tmp->arg[i])));
+				}
+				split_free(for_split);
+				if (flag != 1)
+					exit_exec(data, 0);
 			}
 			else
 			{
-				ft_lstdelone(&tmp_env);
-				ft_lstadd_back(&data->env, ft_lstnew(ft_strdup(tmp->arg[1])));
+				ft_putstr_fd("minishell: export: ", 2);
+				ft_putstr_fd(tmp->arg[1], 2);
+				ft_putstr_fd(": not a valid identifier\n", 2);
+				exit_exec(data, 1);
+				flag = 1;
 			}
-			split_free(for_split);
-			exit_exec(data, 0);
-		}
-		else
-		{
-			ft_putstr_fd("minishell: export: ", 2);
-			ft_putstr_fd(tmp->arg[1], 2);
-			ft_putstr_fd(": not a valid identifier\n", 2);
-			exit_exec(data, 1);
+			i++;
 		}
 	}
 	else
 	{
-
+		export_if_no_arg(data);
+		exit_exec(data, 0);
 	}
 }
 
